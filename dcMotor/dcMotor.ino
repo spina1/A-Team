4f3061dcde1d6bc7ce217f1ef56b1bb8f856e6a9
spina1 <spina1@uno.edu>
2019-11-04 20:39:16 -0600

    Updated Dc Motor Code

---
+++ dcMotor/dcMotor.ino
@@ -0,0 +1,79 @@
+const int IN1 = 5;
+const int IN2 = 4;
+const int ENA = 6;
+
+const int IN3 = 10;
+const int IN4 = 9;
+const int ENB = 11;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+
+  pinMode(IN4, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(ENB, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ramp_up(100);
+Motor1_Brake();
+Motor2_Brake();
+delay(100);
+//Motor1_Forward(150);
+//Motor2_Forward(150);
+//delay(1000);
+//Motor1_Brake();
+//Motor2_Brake();
+//delay(100);
+//Motor1_Backward(200);
+//Motor2_Backward(200);
+//delay(1000);
+}
+void Motor1_Backward(int Speed)
+{
+ digitalWrite(IN1,HIGH);
+ digitalWrite(IN2,LOW);
+ analogWrite(ENA,Speed);
+}
+
+void Motor1_Forward(int Speed)
+{
+ digitalWrite(IN1,LOW);
+ digitalWrite(IN2,HIGH);
+ analogWrite(ENA,Speed);
+}
+void Motor1_Brake()
+{
+ digitalWrite(IN1,LOW);
+ digitalWrite(IN2,LOW);
+}
+void Motor2_Backward(int Speed)
+{
+ digitalWrite(IN3,LOW);
+ digitalWrite(IN4,HIGH);
+ analogWrite(ENB,Speed);
+}
+
+void Motor2_Forward(int Speed)
+{
+ digitalWrite(IN3,HIGH);
+ digitalWrite(IN4,LOW);
+ analogWrite(ENB,Speed);
+}
+void Motor2_Brake()
+{
+ digitalWrite(IN3,LOW);
+ digitalWrite(IN4,LOW);
+}
+
+void ramp_up(int speed){
+float i = 0;
+while (i < speed){
+  Motor1_Forward(i);
+  i = i + .1;
+  delay(10); 
+}
+}
